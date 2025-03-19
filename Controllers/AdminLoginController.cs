78c168775d37d049944878765ffdc7be332cf395
Saurav <bhas0011@algonquinlive.com>
2025-03-19 00:52:57 -0400

    Final Login , Register and MenuItem working

--- Controllers/AdminLoginController.cs
+++ Controllers/AdminLoginController.cs
@@ -33,14 +33,14 @@ namespace RENAPI.Controllers
         [HttpPost("Login")]
         public async Task<IActionResult> Login([FromBody] AdminLoginRequest loginRequest)
         {
-            var user = await _userManager.FindByEmailAsync(loginRequest.Username);
+            var user = await _userManager.FindByEmailAsync(loginRequest.Username ?? string.Empty);
 
             if (user == null)
             {
                 return Unauthorized(new { success = false, message = "Invalid username or password" });
             }
 
-            var result = await _signinManager.CheckPasswordSignInAsync(user, loginRequest.Password, false);
+            var result = await _signinManager.CheckPasswordSignInAsync(user, loginRequest.Password ?? string.Empty, false);
 
             if (!result.Succeeded)
             {
@@ -50,6 +50,11 @@ namespace RENAPI.Controllers
             int userId = user.Id;
             var token = GenerateJWTToken(user);
             var restaurant = await _context.Restaurants.Where(r => r.UserId == userId).FirstOrDefaultAsync();
+
+            if (restaurant == null)
+            {
+                return NotFound(new { success = false, message = "Restaurant not found" });
+            }
 
             int restaurantId = restaurant.RestaurantId;
             string restaurantName = restaurant.RestaurantName;
@@ -71,12 +76,12 @@ namespace RENAPI.Controllers
             var Claims = new[]
             {
                 new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
-                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email ?? string.Empty),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim("Name", user.UserName)
+                new Claim("Name", user.UserName ?? string.Empty)
             };
 
-            var Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtKey));
+            var Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtKey ?? string.Empty));
 
             var Credentials = new SigningCredentials(Key, SecurityAlgorithms.HmacSha256);
 
--- obj/Debug/net8.0/RENAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/RENAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RENAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc9371c786bec8cfa0bcf8d332dd65f78af05f05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+265569cfd11d5328432e8820c25331c183682841")]
 [assembly: System.Reflection.AssemblyProductAttribute("RENAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RENAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
