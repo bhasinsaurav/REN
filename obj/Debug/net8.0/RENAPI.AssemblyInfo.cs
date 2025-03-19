0ad0cc381e764cfe2fdda2bdac29d75ad90fbc5f
Saurav <bhas0011@algonquinlive.com>
2025-03-19 01:00:42 -0400

    Ignore commit

--- obj/Debug/net8.0/RENAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/RENAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RENAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e1dec3ed552d08b0cad3b4642ce2a6046ffcfd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef4facc955828dbf80fdeb9951eeb013414a82eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RENAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RENAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
